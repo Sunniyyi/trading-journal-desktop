9be91184441c4c36c0a2ffc9e5473f122d2684d9
Sunniyyi <noamech59@gmail.com>
2026-08-15 22:31:54 +0200

    fix(updater): expose version and config controls to UI

--- src/preload.js
+++ src/preload.js
@@ -15,15 +15,17 @@ ipcRenderer.on('desktop:update-status', (_event, status) => renderUpdateHud(stat
 contextBridge.exposeInMainWorld('desktopApp', {
   isDesktop: true,
   platform: process.platform,
-  version: '1.2.0',
+  version: '1.3.0',
   openFxReplay: () => ipcRenderer.invoke('desktop:open-fxreplay'),
   openDataFolder: () => ipcRenderer.invoke('desktop:open-data-folder'),
   importBackup: () => ipcRenderer.invoke('desktop:choose-backup'),
+  getAppVersion: () => ipcRenderer.invoke('desktop:get-app-version'),
   getUpdateStatus: () => ipcRenderer.invoke('desktop:get-update-status'),
   checkForUpdates: () => ipcRenderer.invoke('desktop:check-update'),
   startUpdate: () => ipcRenderer.invoke('desktop:start-update'),
   restartForUpdate: () => ipcRenderer.invoke('desktop:restart-update'),
   openUpdateCenter: () => ipcRenderer.invoke('desktop:open-update-center'),
+  openUpdateConfig: () => ipcRenderer.invoke('desktop:open-update-config'),
   onUpdateStatus: (callback) => {
     if (typeof callback !== 'function') return () => {};
     const listener = (_event, status) => callback(status || {});
