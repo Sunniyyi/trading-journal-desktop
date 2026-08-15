e06e63cf98835d04593716a05b3fda5ee12be41d
Sunniyyi <noamech59@gmail.com>
2026-08-16 00:03:48 +0200

    build(renderer): load journal premium layer

--- scripts/renderer-transform.js
+++ scripts/renderer-transform.js
@@ -103,6 +103,7 @@ function injectDesktopStyles(source) {
     '<link rel="stylesheet" href="./ui/widget-polish.css">',
     '<link rel="stylesheet" href="./ui/typography-polish.css">',
     '<link rel="stylesheet" href="./ui/journal-polish.css">',
+    '<link rel="stylesheet" href="./ui/journal-premium.css">',
     '<link rel="stylesheet" href="./ui/workspace-routing-fix.css">'
   ].join('\n');
   if (!source.includes(marker)) throw new Error('Renderer transform: </head> introuvable.');
