c73ecc2b846b3c8c37fcffc90e7b097762bd29e8
Sunniyyi <noamech59@gmail.com>
2026-08-15 21:56:30 +0200

    build(ui): load the 2.1.4 layout layer last

--- scripts/renderer-transform.js
+++ scripts/renderer-transform.js
@@ -94,7 +94,8 @@ function injectDesktopStyles(source) {
     '<link rel="stylesheet" href="./ui/components.css">',
     '<link rel="stylesheet" href="./ui/workspaces.css">',
     '<link rel="stylesheet" href="./ui/stability.css">',
-    '<link rel="stylesheet" href="./ui/visual-hotfix.css">'
+    '<link rel="stylesheet" href="./ui/visual-hotfix.css">',
+    '<link rel="stylesheet" href="./ui/layout-v214.css">'
   ].join('\n');
   if (!source.includes(marker)) throw new Error('Renderer transform: </head> introuvable.');
   return source.replace(marker, links + '\n' + marker);
