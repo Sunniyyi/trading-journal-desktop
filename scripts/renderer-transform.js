2c203d6ff5b3e8e46a441ff126a35fed93502b4b
Sunniyyi <noamech59@gmail.com>
2026-08-17 22:53:35 +0200

    build(renderer): load simulations premium layer

--- scripts/renderer-transform.js
+++ scripts/renderer-transform.js
@@ -112,6 +112,7 @@ function injectDesktopStyles(source) {
     '<link rel="stylesheet" href="./ui/scan-premium-v220.css">',
     '<link rel="stylesheet" href="./ui/scan-guide-hotfix-v221.css">',
     '<link rel="stylesheet" href="./ui/backtest-overview-v222.css">',
+    '<link rel="stylesheet" href="./ui/backtest-simulations-v223.css">',
     '<link rel="stylesheet" href="./ui/backtest-state-guard.css">',
     '<link rel="stylesheet" href="./ui/workspace-routing-fix.css">'
   ].join('\n');
